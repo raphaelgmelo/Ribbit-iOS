04a1d70b188455304807b852acf024932fd952df
Raphael Melo <raphaelgmelo@gmail.com>
2015-10-15 08:33:53 -0300

    auth screens + navigation

---
+++ Ribbit/Ribbit/InboxViewController.h
@@ -0,0 +1,13 @@
+//
+//  InboxTableViewController.h
+//  Ribbit
+//
+//  Created by Raphael Melo on 15/10/15.
+//  Copyright © 2015 raphaelgmelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InboxViewController : UITableViewController
+
+@end
---
+++ Ribbit/Ribbit/InboxViewController.m
@@ -0,0 +1,37 @@
+//
+//  InboxTableViewController.m
+//  Ribbit
+//
+//  Created by Raphael Melo on 15/10/15.
+//  Copyright © 2015 raphaelgmelo. All rights reserved.
+//
+
+#import "InboxViewController.h"
+
+@interface InboxViewController ()
+
+@end
+
+@implementation InboxViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self performSegueWithIdentifier:@"showLogin" sender:self];
+    
+}
+
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Incomplete implementation, return the number of sections
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete implementation, return the number of rows
+    return 0;
+}
+
+@end
