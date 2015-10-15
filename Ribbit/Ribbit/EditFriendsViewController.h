06882a59547b98a35f1891fee9db33ac7ad247b4
Raphael Melo <raphaelgmelo@gmail.com>
2015-10-15 18:00:55 -0300

    friends list

---
+++ Ribbit/Ribbit/EditFriendsViewController.h
@@ -0,0 +1,16 @@
+//
+//  EditFriendsTableViewController.h
+//  Ribbit
+//
+//  Created by Raphael Melo on 15/10/15.
+//  Copyright © 2015 raphaelgmelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditFriendsViewController : UITableViewController
+
+
+@property (nonatomic, strong) NSArray *allUsers;
+
+@end
---
+++ Ribbit/Ribbit/EditFriendsViewController.m
@@ -0,0 +1,66 @@
+//
+//  EditFriendsTableViewController.m
+//  Ribbit
+//
+//  Created by Raphael Melo on 15/10/15.
+//  Copyright © 2015 raphaelgmelo. All rights reserved.
+//
+
+#import "EditFriendsViewController.h"
+#import <Parse/Parse.h>
+
+@interface EditFriendsViewController ()
+
+@end
+
+@implementation EditFriendsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    PFQuery *query = [PFUser query];
+    [query orderByAscending:@"username"];
+    
+    [query findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable error) {
+       
+        
+        if (error){
+            NSLog(@"Error: %@ %@", error, [error userInfo]);
+        }
+        else{
+            self.allUsers = objects;
+            
+            // Update table view
+            [self.tableView reloadData];
+        }
+        
+        
+    }];
+
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.allUsers.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath{
+    
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    PFUser *user = [self.allUsers objectAtIndex:indexPath.row];
+    
+    cell.textLabel.text = user.username;
+    
+    return cell;
+    
+}
+
+
+@end
