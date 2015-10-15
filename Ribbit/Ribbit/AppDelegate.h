0c3e23a30c15e8ab2f1d2c3a956837d02453beb0
Raphael Melo <raphaelgmelo@gmail.com>
2015-10-15 07:39:37 -0300

    tab bar navigation

---
+++ Ribbit/Ribbit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ribbit
+//
+//  Created by Raphael Melo on 15/10/15.
+//  Copyright © 2015 raphaelgmelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
